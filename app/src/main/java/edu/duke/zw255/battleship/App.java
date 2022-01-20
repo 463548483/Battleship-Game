d396e9f225ddda9544c83e44e35097e3bab7630c
zw255 <zijingwang1995@outlook.com>
2022-01-19 22:46:06 -0500

    9

--- app/src/main/java/edu/duke/zw255/battleship/App.java
+++ app/src/main/java/edu/duke/zw255/battleship/App.java
@@ -3,12 +3,52 @@
  */
 package edu.duke.zw255.battleship;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Reader;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+  final Board<Character> theBoard;
+      final BoardTextView view;
+      final BufferedReader inputReader;
+      final PrintStream out;
+  public App(){
+    theBoard=new BattleShipBoard(10,20);
+    view=new BoardTextView(theBoard);
+    inputReader=new BufferedReader(new InputStreamReader(System.in));
+    out=System.out;
+  }
+
+  public App(Board<Character> theBoard, Reader inputSource, PrintStream out) {
+    this.theBoard = theBoard;
+    this.view = new BoardTextView(theBoard);
+    this.inputReader = new BufferedReader(inputSource);
+    this.out = System.out;
+  }
+
+  public Placement readPlacement(String prompt) throws IOException {
+    out.println(prompt);
+    String s = inputReader.readLine();
+    return new Placement(s);
+  }
+
+  public void doOnePlacement() throws IOException{
+    readPlacement("Where would you like to put your ship?");
+    BasicShip p1=new BasicShip(new Coordinate("A1"));
+    theBoard.tryAddShip(p1);
+    out.println(theBoard);
+    
+  }
+  
+  public String getGreeting() {
+      return "";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String[] args) throws IOException {
+      Board b=new BattleShipBoard<>(10, 20);
+      App app=new App(b,new InputStreamReader(System.in),System.out);
+      app.doOnePlacement();
     }
 }
